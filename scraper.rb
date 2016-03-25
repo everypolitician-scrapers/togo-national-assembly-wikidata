e541f4e0a9869a7b6208260aa586b5ffac187d9e
Tony Bowden <tony@tmtm.com>
2016-03-25 12:52:28 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -2,11 +2,9 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-require 'pry'
 
 fr_names = WikiData::Category.new( 'Catégorie:Personnalité politique togolaise', 'fr').member_titles
 en_names = WikiData::Category.new( 'Category:Members of the National Assembly (Togo)', 'en').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, fr: fr_names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
 
